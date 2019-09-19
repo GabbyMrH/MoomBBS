f5858be22675196df79dbbf6eb865c0cd541d3e4
vagrant <vagrant@homestead>
2019-09-19 09:43:14 +0000

    注册界面验证码

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -41,6 +41,7 @@ class RegisterController extends Controller
     }
 
     /**
+     * 添加表单验证
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
@@ -48,10 +49,16 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        //Validator 表单验证的 make() 方法第三个参数是自定义错误提示
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            //表达式里的第二个 captcha 是 mews/captcha 自定义的表单验证规则
+            'captcha'=>['required','captcha'],
+            ],[
+                'captcha.required'=>'验证码不能为空',
+                'captcha.captcha'=>'验证码不正确',
         ]);
     }
 
