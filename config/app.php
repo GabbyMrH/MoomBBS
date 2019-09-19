93fd49ab757fbf1cd82570b1128884c0160740a8
vagrant <vagrant@homestead>
2019-09-19 09:16:16 +0000

    生成用户认证代码

--- config/app.php
+++ config/app.php
@@ -158,7 +158,9 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        //Illuminate\Translation\TranslationServiceProvider::class,
+        //上面语言替换为
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
