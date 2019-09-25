9484d58e787aa1ad8f09d96265bd872bbb1a2167
vagrant <vagrant@homestead>
2019-09-25 04:07:04 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -21,10 +22,20 @@ class UsersController extends Controller
     }
     //更新个人资料-调用了FormRequest请求验证用户提交数据
     //ps:上传文件要在form表单添加enctype="multipart/form-data"
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-        dd($request->avatar);
-        $user->update($request->all());
+        //$data = $request->all(); 赋值 $data 变量，以便对更新数据的操作
+        $data = $request->all();
+        //验证上传文件
+        if($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id);
+            //将返回参数插入data数组，用于更新路径
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        //更新数据
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 
