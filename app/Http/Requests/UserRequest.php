3dc3a4d923232a5988537523b8405fae56a00809
vagrant <vagrant@homestead>
2019-09-25 02:55:10 +0000

    编辑个人资料

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * 本功能用于权限验证(这里暂时用不着，所以设为true)
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //验证规则 -Auth::id() 指示将此 ID 排除在外。
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80',
+        ];
+    }
+    //该信息提示方法为FormRequest自带的，此处重写即可
+    public function messages()
+    {
+        return [
+            'name.unique' => '用户名已被占用，请重新填写',
+            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
+            'name.between' => '用户名必须在 3 - 25 个字符之间。',
+            'name.required' => '用户名不能为空。',
+        ];
+    }
+}
---
+++ database/migrations/2019_09_25_101950_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //新增字段
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //移除字段
+            $table->dropColumn('avatr');
+            $table->dropColumn('introduction');
+        });
+    }
+}
