74e2f61770bdffd94740d1f21e2bc23c2e0cf657
vagrant <vagrant@homestead>
2019-09-25 09:34:05 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //用户与话题一对多关联
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
