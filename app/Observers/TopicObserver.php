d745980eba6d659581258cf487e53ff80834fe73
vagrant <vagrant@homestead>
2019-09-25 12:26:41 +0000

    修复xss注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,9 @@ class TopicObserver
     //在 Topic 模型保存时触发的 saving 事件
     public function saving(Topic $topic)
     {
+        //过滤器
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         //make_excerpt()为自定义辅助函数-需要在helpers.php中添加
         $topic->excerpt = make_excerpt($topic->body);
     }
