805540c988c55767bd7d6d19147ff4cbcfc95ddf
vagrant <vagrant@homestead>
2019-09-25 07:28:54 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //注册用户资源路由
 Route::resource('users', 'UsersController',['only'=>['show','update','edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
