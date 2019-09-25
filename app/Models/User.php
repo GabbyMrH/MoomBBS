fcca98fe9bd37158a71653d26bda9458b3bd9cd7
vagrant <vagrant@homestead>
2019-09-25 12:54:10 +0000

    用户只能删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    //用户编辑认证
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -8,14 +8,14 @@
   <div class="row">
 
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
-      <div class="card ">
+      <div class="card shadow-lg bg-white">
         <div class="card-body">
           <div class="text-center">
             作者：{{ $topic->user->name }}
           </div>
           <hr>
           <div class="media">
-            <div align="center">
+            <div align="center" >
               <a href="{{ route('users.show', $topic->user->id) }}">
                 <img class="thumbnail img-fluid" src="{{ $topic->user->avatar }}" width="300px" height="300px">
               </a>
@@ -26,7 +26,7 @@
     </div>
 
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-      <div class="card ">
+      <div class="card shadow-lg bg-white">
         <div class="card-body">
           <h1 class="text-center mt-3 mb-3">
             {{ $topic->title }}
@@ -43,15 +43,23 @@
             {!! $topic->body !!}
           </div>
 
-          <div class="operate">
-            <hr>
-            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-edit"></i> 编辑
-            </a>
-            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-trash-alt"></i> 删除
-            </a>
-          </div>
+          @can('update', $topic)
+            <div class="operate">
+              <hr>
+              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> 编辑
+              </a>
+              <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                    style="display: inline-block;"
+                    onsubmit="return confirm('您确定要删除吗？');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+              </form>
+            </div>
+          @endcan
 
         </div>
       </div>
