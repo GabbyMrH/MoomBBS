60abba91fd147e963b1e22e9816688ff29ecd6bd
vagrant <vagrant@homestead>
2019-09-25 02:16:55 +0000

    用户个人页面模型

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-left">
-      由 <a href="https//github.com/GabbyMrH" target="_blank">GabbyMrH</a> 设计和编码
+      由 <a href="https://github.com/GabbyMrH" target="_blank">GabbyMrH</a> 设计和编码
     </p>
 
     <p class="float-right"><a href="mailto:gabbymrh@gmail.com">联系我们</a></p>
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //<=
 
+//注册用户资源路由
+Route::resource('users', 'UsersController',['only'=>['show','update','edit']]);
+
