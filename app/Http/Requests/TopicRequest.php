7e1d74de3872b278631ab3b5372949d719367973
vagrant <vagrant@homestead>
2019-09-25 10:11:22 +0000

    用户可以创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -10,19 +10,17 @@ class TopicRequest extends Request
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
+            //UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
+
             case 'GET':
             case 'DELETE':
             default:
@@ -34,8 +32,10 @@ class TopicRequest extends Request
 
     public function messages()
     {
+        //自定提示信息
         return [
-            // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,4 +18,11 @@ class TopicObserver
     {
         //
     }
+
+    //在 Topic 模型保存时触发的 saving 事件
+    public function saving(Topic $topic)
+    {
+        //make_excerpt()为自定义辅助函数-需要在helpers.php中添加
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
