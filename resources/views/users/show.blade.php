9de5417e79c78dcb3fc29d3011ed149d48aaa839
vagrant <vagrant@homestead>
2019-09-25 04:12:33 +0000

    显示头像

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,7 @@
 
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
     <div class="card shadow-lg bg-white">
-      <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
+      <img class="card-img-top" src="{{$user->avatar}}" alt="{{ $user->name }}">
       <div class="card-body">
             <h5><strong>个人简介</strong></h5>
             {{-- 此处执行后仍然不显示，数据表内也没数据，原因是还需要在User模型中将introduction字段加入过滤(允许修改) --}}
