1cc349aeee3c51caa366f3b60ef6bc0430542577
vagrant <vagrant@homestead>
2019-09-26 01:40:45 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -26,12 +26,19 @@ class TopicObserver
         //xss过滤器
         $topic->body = clean($topic->body, 'user_topic_body');
 
-        //make_excerpt()为自定义辅助函数-需要在helpers.php中添加
+        //生成话题摘录 make_excerpt()为自定义辅助函数-需要在helpers.php中添加
         $topic->excerpt = make_excerpt($topic->body);
 
+    }
+
+    //
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
