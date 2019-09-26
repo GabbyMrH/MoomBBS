4e41a500129fe54c26ad2c976cd127bac76080db
vagrant <vagrant@homestead>
2019-09-26 14:21:27 +0000

    集成administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -43,6 +43,14 @@
             {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            {{-- 判断用户权限是否显示后台管理 --}}
+              @can('manage_contents')
+              <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+                <i class="fas fa-tachometer-alt mr-2"></i>
+                后台管理
+              </a>
+              <div class="dropdown-divider"></div>
+            @endcan
             <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}"><i class="far fa-user mr-2"></i>个人中心</a>
             <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}"><i class="far fa-edit mr-2"></i>编辑资料</a>
             <div class="dropdown-divider"></div>
