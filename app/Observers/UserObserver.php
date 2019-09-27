e22185474904f4b3dab10ca2d0c99bee93358995
vagrant <vagrant@homestead>
2019-09-27 07:18:32 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,13 @@ class UserObserver
     {
         //
     }
+
+    //指定用户默认头像
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = config('app.url') . '/uploads/images/default/default-photo.png';
+        }
+    }
 }
